5ca1c5d1725a55dd42154ee1bb69ecd213a6d9d5
Devaure <daviddevaure@gmail.com>
2022-01-23 18:56:28 +0100

    create method onDelete, onEdit, onSelect and create onActionEventItem to emit event to product

--- src/app/components/products/products-list/product-item/product-item.component.ts
+++ src/app/components/products/products-list/product-item/product-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
-import { ActionEvent } from 'src/app/state/product.state';
+import { ActionEvent, ProductActionTypes } from 'src/app/state/product.state';
 
 @Component({
   selector: 'app-product-item',
@@ -18,14 +18,14 @@ export class ProductItemComponent implements OnInit {
   }
 
   onSelect(product:Product){
-    
+    this.itemEventEmitter.emit({type:ProductActionTypes.SELECT_PRODUCT, payload:product});
   }
 
   onDelete(product:Product){
-
+    this.itemEventEmitter.emit({type:ProductActionTypes.DELETE_PRODUCT, payload:product});
   }
 
   onEdit(product:Product){
-
+    this.itemEventEmitter.emit({type:ProductActionTypes.EDIT_PRODUCT, payload:product});
   }
 }
