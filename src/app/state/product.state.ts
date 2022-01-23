d610270d4cef82cbb7354d3733c14248e44e45e6
Devaure <daviddevaure@gmail.com>
2022-01-23 16:27:23 +0100

    create an enum for my productEventEmitter

--- src/app/state/product.state.ts
+++ src/app/state/product.state.ts
@@ -4,6 +4,14 @@ export enum DataStateEnum {
     ERROR
 }
 
+export enum ProductActionTypes{
+    GET_ALL_PRODUCTS="[Product] Get All Product",
+    GET_SELECTED_PRODUCTS="[Product] Get All selected Product",
+    GET_AVAILABLE_PRODUCTS="[Product] Get All available Product",
+    SEARCH_PRODUCTS="[Product] Search Product",
+    NEW_PRODUCTS="[Product] New Product",
+}
+
 
 export interface AppDataState<T>{
     dataState?: DataStateEnum,
